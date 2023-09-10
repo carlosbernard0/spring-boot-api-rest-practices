8e9085e2899fd9e8c7f3e42c381a03f961b7efed
carlosbernard0 <carlosbernardo774@gmail.com>
2023-09-10 19:27:48 -0300

    Bean validation + método Get

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,16 +1,17 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.MedicoEntity;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -21,8 +22,12 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new MedicoEntity(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedicos> listar(){
+        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
     }
 }
